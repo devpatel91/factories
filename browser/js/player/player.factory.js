217e3cf58bcc1485b9ed2298d0428b5bf428a821
devpatel91 <devnitinpatel91@gmail.com>
2016-08-24 11:50:32 -0400

    bored

--- browser/js/player/player.factory.js
+++ browser/js/player/player.factory.js
@@ -1,5 +1,45 @@
 'use strict';
+var audio = document.createElement('audio');
+juke.factory('PlayerFactory', function($http) {
+    var songPlaying = false;
+    var retObj = {
 
-juke.factory('PlayerFactory', function(){
-  // non-UI logic in here
+        start: function(song) {
+            if (songPlaying) {
+                this.pause();
+            }
+            audio.src = song.audioUrl;
+            audio.load();
+            audio.play();
+            songPlaying = true;
+
+
+        },
+        pause: function() {
+            audio.pause();
+            songPlaying = false;
+        },
+        resume: function(){
+        	audio.play();
+        	songPlaying = true;
+        },
+        isPlaying : function(){
+        	return songPlaying;
+        },
+        getCurrentSong: function(){
+
+        	if(songPlaying || !songPlaying){
+        	return  this.song;
+        	}else{
+        		return null;
+        	} 
+        	
+        }
+
+
+
+
+
+    }
+    return retObj;
 });
